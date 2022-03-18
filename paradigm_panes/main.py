ce72e0b2c4a0946d4da643e965f8838fc49b1b60
Uladzimir Bondarau <bondarau@ualberta.ca>
2022-03-18 02:03:14 -0400

    setting up basic paradigm manager with downloaded fst generator

--- paradigm_panes/main.py
+++ paradigm_panes/main.py
@@ -1,3 +1,11 @@
 """
     Planning to make it a central place to run paradigm generation
 """
+from generation import default_paradigm_manager
+
+def main():
+    print("Hello World!")
+    paradigm_manager = default_paradigm_manager()
+
+if __name__ == "__main__":
+    main()
