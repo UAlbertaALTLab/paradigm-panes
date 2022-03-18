49c454c791242643eb4633f07e88781ccac5114f
Uladzimir Bondarau <bondarau@ualberta.ca>
2022-03-18 17:42:10 -0400

    partial serialize. need to include inflections source

--- paradigm_panes/main.py
+++ paradigm_panes/main.py
@@ -31,6 +31,7 @@ def main():
                 size = default_size
 
         paradigm = paradigm_for(paradigm_manager, paradigm_type, lemma, size)
+        paradigm = serialize_paradigm(paradigm)
         print(paradigm)
 
 def paradigm_for(paradigm_manager: ParadigmManager, paradigm_type: str, fst_lemma: str, paradigm_size: str) -> Optional[Paradigm]:
@@ -47,6 +48,71 @@ def paradigm_for(paradigm_manager: ParadigmManager, paradigm_type: str, fst_lemm
             return paradigm
 
     return None
+
+# TODO cleanup for package use
+def serialize_paradigm(paradigm: Paradigm) -> Dict[str, any]:
+    """
+    Serializes a Paradigm object as a dictionary
+
+    :param paradigm: the paradigm to be serialized
+    :return: dictionary representation
+    """
+    panes = []
+
+    for pane in (paradigm.panes or []):
+
+        tr_rows = []
+        for row in (pane.rows or []):   # potential problem: we don't distinguish between compound rows and non-compound rows. this might be a problem for the frontend.
+            if row.is_header:
+                tr_rows.append({
+                    "is_header": True,
+                    "label": row.fst_tags,
+                    "cells": []
+                })
+            else:
+                cells = []
+                for cell in (row.cells or []):
+                    cell_data = {
+                        "should_suppress_output": cell.should_suppress_output,
+                        "is_label": cell.is_label,
+                        "is_inflection": cell.is_inflection,
+                        "is_missing": cell.is_missing,
+                        "is_empty": cell.is_empty
+                    }
+
+                    if cell_data["is_label"]:
+                        cell_data["label_for"] = cell.label_for
+                        cell_data["label"] = cell.fst_tags
+
+                        if type(cell_data["label_for"]) != str:  # if cell.label_for was never instantiated
+                            cell_data["label_for"] = ""
+                        if cell_data["label_for"] == "row":
+                            cell_data["row_span"] = cell.row_span
+
+                    elif cell_data["is_inflection"] and not cell_data["is_missing"]:
+                        # TODO add orth
+                        # cell_data["inflection"] = orth(cell.inflection)
+                        cell_data["inflection"] = cell.inflection
+                        cell_data["recording"] = cell.recording
+
+                        # TODO check wordforms?
+                        # if cell.inflection in observed_wordforms():
+                        #     cell_data["observed"] = True
+                        # else:
+                        #     cell_data["observed"] = False
+                        cell_data["observed"] = False
+
+                    cells.append(cell_data)
+
+                tr_rows.append({
+                    "is_header": False,
+                    "label": None,  # shouldn't be used.
+                    "cells": cells
+                })
+
+        panes.append({"tr_rows": tr_rows})
+
+    return {"panes": panes}
 
 if __name__ == "__main__":
     main()
